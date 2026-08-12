5f31d2627cd1b7ea6e0a5da18ba334ef573b07a3
Jacff <320452949@qq.com>
2026-08-12 09:45:59 +0800

    1.UI框架搭建

---
+++ UIFramework/Assets/UIFramework/Extention/DictionaryExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DictionaryExtension { 
+   
+    public static TValue TryGet<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+    {
+        TValue value;
+
+        dict.TryGetValue(key, out value);
+
+        return value;
+    }
+
+}
---
+++ UIFramework/Assets/UIFramework/Panel/KnapsackPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnapsackPanel : BasePanel
+{
+    private CanvasGroup _canvasGroup;
+
+    public void OnClosePanel()
+    {
+        UIManager.Instance.PopPanel();
+    }
+
+    public void EnterMessage()
+    {
+        UIManager.Instance.PushPanel(UIPanelType.Message);
+    }
+
+    private void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public override void OnEnter()
+    {
+        if (_canvasGroup == null)
+            _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 1.0f;
+        _canvasGroup.blocksRaycasts = true;
+    }
+
+    public override void OnExit()
+    {
+        _canvasGroup.alpha = 0.0f;
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public override void OnPause()
+    {
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public override void OnResume()
+    {
+        _canvasGroup.blocksRaycasts = true;
+    }
+}
